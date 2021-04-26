d5d864c50c630a1d5e68ff51b3efe9ef46ad3871
longht <longht@runsystem.net>
2021-04-26 15:27:29 +0700

    database

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -4,7 +4,6 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        uri: env('mongodb+srv://jonhon:Long123456.@cluster1.8r81q.mongodb.net/next-database?retryWrites=true&w=majority'),
         host: env('DATABASE_HOST', '127.0.0.1'),
         srv: env.bool('DATABASE_SRV', false),
         port: env.int('DATABASE_PORT', 27017),
@@ -13,8 +12,6 @@ module.exports = ({ env }) => ({
         password: env('DATABASE_PASSWORD', null),
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', false),
       },
     },
   },
